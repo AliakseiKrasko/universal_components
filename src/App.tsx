79529d235013e01eb6f2ddb89743a94e9ca191d8
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-05-13 12:08:06 +0300

    added storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import {Header} from "./components/Header/Header.tsx";
 import {DatePicker} from "./components/DatePickerSingle/DatePicker/DatePicker.tsx";
+import {Select} from "./components/Select/Select.tsx";
+import {useState} from "react";
+import {Typography} from "./components/typography/typography.tsx";
+
+
+
+
+const items = [
+    { value: "item1", label: "Item 1" },
+    { value: "item2", label: "Item 2" },
+    { value: "item3", label: "Item 3" },
+];
 
 function App() {
+    const [selectedValue, setSelectedValue] = useState<string | undefined>();
+
+    const handleValueChange = (newValue: string) => {
+        setSelectedValue(newValue);
+    };
+
     return <div>
         <Header />
 
-        <DatePicker mode="single" onDateChange={(val) => console.log(val)} />
+        <DatePicker mode="range" />
+
+
+        <Select
+            label="Select Item"
+            placeholder="Choose an option"
+            value={selectedValue}
+            onValueChange={handleValueChange}
+            items={items}
+            groupLabel="Group 1"
+            withSeparator={true}
+        />
+        {selectedValue && <Typography variant="regular_14">Selected: {selectedValue}</Typography>}
 
     </div>
 }
---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,86 @@
+import s from "./select.module.css";
+import { ChevronDownIcon, ChevronUpIcon } from "@radix-ui/react-icons";
+import * as RadixSelect from "@radix-ui/react-select";
+import { clsx } from "clsx";
+
+import {type ComponentPropsWithoutRef, type ComponentRef, forwardRef, useId} from "react";
+import type {NullableProps} from "../types/NullableProps.ts";
+import type {SelectItemsProps} from "../types/SelectItemsProps.ts";
+import {SelectItem} from "./SelectItems/selectItems.tsx";
+import { Typography } from "../typography/typography.tsx";
+
+export const Select = forwardRef<ComponentRef<typeof RadixSelect.Trigger>, SelectProps>(
+    (
+        { className, placeholder, defaultValue, value, label, disabled, items, groupLabel, withSeparator = true, ...rest },
+        ref
+    ) => {
+        const generatedId = useId();
+        const id = rest.id || generatedId;
+
+        return (
+            <div className={s.selectWrapper}>
+                {label && (
+                    <Typography variant={"regular_14"} color={"dark"} asChild>
+                        <label htmlFor={id} className={s.label}>
+                            {label}
+                        </label>
+                    </Typography>
+                )}
+                <RadixSelect.Root
+                    defaultValue={defaultValue}
+                    value={value}
+                    onValueChange={rest.onValueChange}
+                    disabled={disabled}
+                >
+                    <RadixSelect.Trigger id={id} className={clsx(s.trigger, className)} ref={ref} {...rest}>
+                        <RadixSelect.Value placeholder={placeholder} />
+                        <RadixSelect.Icon>
+                            <ChevronDownIcon className={s.iconDown} />
+                        </RadixSelect.Icon>
+                    </RadixSelect.Trigger>
+
+                    <RadixSelect.Portal>
+                        <RadixSelect.Content className={s.Content} position={"popper"}>
+                            <RadixSelect.ScrollUpButton className={s.ScrollButton}>
+                                <ChevronUpIcon />
+                            </RadixSelect.ScrollUpButton>
+                            <RadixSelect.Viewport className={s.Viewport}>
+                                <RadixSelect.Group>
+                                    {groupLabel && (
+                                        <>
+                                            <RadixSelect.Label style={{ marginLeft: 5 }}>{groupLabel}</RadixSelect.Label>
+                                            {withSeparator && <RadixSelect.Separator className={s.Separator} />}
+                                        </>
+                                    )}
+                                    {items.map((item) => (
+                                        <SelectItem key={item.value} value={item.value}>
+                                            <Typography variant={"regular_14"} className={s.selectItems}>
+                                                {item.icon && item.icon} {item.label}
+                                            </Typography>
+                                        </SelectItem>
+                                    ))}
+                                </RadixSelect.Group>
+                            </RadixSelect.Viewport>
+                            <RadixSelect.ScrollDownButton>
+                                <ChevronDownIcon />
+                            </RadixSelect.ScrollDownButton>
+                        </RadixSelect.Content>
+                    </RadixSelect.Portal>
+                </RadixSelect.Root>
+            </div>
+        );
+    }
+);
+
+export type SelectProps = {
+    id?: string;
+    className?: string;
+    labelClassName?: string;
+    placeholder?: string;
+    label?: string;
+    groupLabel?: NullableProps<string>;
+    withSeparator?: boolean;
+    items: SelectItemsProps[];
+} & ComponentPropsWithoutRef<typeof RadixSelect.Root>;
+
+Select.displayName = "Select";
